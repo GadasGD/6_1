b56378c8ac8eb3362c6dcef535853b898b8ddcf8
gadas <gadas4415@gmail.com>
2023-10-10 14:47:48 +0700

    Final

--- main.c
+++ main.c
@@ -1,16 +1,18 @@
+// Output of Perfect numbers up to 10000
 #include <stdio.h>
 
 int main()
 {   
-    int sum = 0;
-    for (int i = 1; i < 10000; i++){
-        for(int d = 1;d < i; d++){
-            if (d % i == 0){
-                sum += 1;
+    
+    for (int i = 3; i < 10000; i++){
+        int sum = 1;
+        for(int d = 2;d < i; d++){
+            if (i % d == 0){
+                sum += d;
             }
         }
         if (sum == i){
-            printf("%d", i);
+            printf("%d\n", i);
         }
     }
     return 0;
